8a502d80653a8a50de790615b3232f89a08fb949
EduardoTakeiYaginuma <eduyagnuma10@gmail.com>
2024-11-09 09:22:52 -0300

    dev

--- src/main/java/insper/pf/controller/CarroController.java
+++ src/main/java/insper/pf/controller/CarroController.java
@@ -1,37 +1,34 @@
 package insper.pf.controller;
 
 import insper.pf.classes.Carro;
-import insper.pf.repositorys.CarroRepository;
 import insper.pf.service.CarroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
 @RequestMapping("/carro")
 public class CarroController {
 
     @Autowired
-    private CarroRepository carroRepository;
-
-    @Autowired
     private CarroService carroService;
 
-    @PostMapping
-    public Carro criarCarro(Carro carro) {
+    // Endpoint para criar um carro
+    @PostMapping("/criar")
+    public Carro criarCarro(@RequestBody Carro carro) {
         return carroService.criarCarro(carro);
     }
-    @PostMapping
+
+    // Endpoint para associar um carro a um dono usando o ID do carro e o CPF do dono
+    @PostMapping("/associar/{idCarro}/{cpf}")
     public Carro associarCarroDono(@PathVariable String idCarro, @PathVariable String cpf) {
         return carroService.associarCarroDono(idCarro, cpf);
     }
 
-    @GetMapping("/{id}")
+    // Endpoint para listar todos os carros de um dono específico pelo ID do dono
+    @GetMapping("/listar/{id}")
     public List<Carro> listaCarros(@PathVariable String id) {
         return carroService.listaCarros(id);
     }
-
-
 }
